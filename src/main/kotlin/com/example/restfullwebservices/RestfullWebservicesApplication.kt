67012823fef48cd351e7ab128b8b565be18fc57b
Paulo Enoque <paulo.enoque@cc-g.com>
2023-11-02 21:01:46 +0200

    Sending initial code to github

---
+++ src/main/kotlin/com/example/restfullwebservices/RestfullWebservicesApplication.kt
@@ -0,0 +1,11 @@
+package com.example.restfullwebservices
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestfullWebservicesApplication
+
+fun main(args: Array<String>) {
+    runApplication<RestfullWebservicesApplication>(*args)
+}
